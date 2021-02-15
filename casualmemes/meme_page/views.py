b19f67bcaf6ccea8fb01cfbf1aa897f3689dc697
Synnysyn <69205616+Synnysyn@users.noreply.github.com>
2021-02-15 15:58:48 +0100

    testing

--- casualmemes/meme_page/views.py
+++ casualmemes/meme_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
 # Create your views here.
+
+
+class MenuView(View):
+    def get(self, request):
+        
+        return render(request, "meme_page/menu.html")
